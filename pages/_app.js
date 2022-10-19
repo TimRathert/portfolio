e371c529f32f5cc64b8d9e5de8b9e8a9f7ace315
TimRathert <rathertt@gmail.com>
2022-10-19 14:55:10 -0400

    working with parallax

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { ParallaxProvidxer } from 'react-scroll-parallax'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ParallaxProvidxer scrollAxis='vertical'>
+      <Component {...pageProps} />
+    </ParallaxProvidxer>
+  )
 }
 
 export default MyApp
