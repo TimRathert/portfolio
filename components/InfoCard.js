8095ef4a145f8eecac5df61028e5c6bf174aee46
TimRathert <rathertt@gmail.com>
2022-10-20 17:34:21 -0400

    tweak infocard for mobile formatting

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 function InfoCard(props) {
   return (
-    <div ref={props.infoDiv} className={`h-fit w-full lg:mx-0 p-4 mt-32 mb-[200px] flex flex-row rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
+    <div ref={props.infoDiv} className={`h-fit w-full lg:mx-0 p-4 mt-32 mb-[200px] flex flex-col sm:flex-row rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
     <Image src='/me.jpg'height={`400px`} width={`400px`} className={`rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.3)]`}/>
 
     <div className='text-color6 bg-fixed bg-containerbg w-full rounded-xl p-2 m-1 flex-col place-content-center sepia-[50%]'>
