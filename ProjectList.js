8d387b2728660867ed7ee3bf3e7567bb962cb49e
TimRathert <rathertt@gmail.com>
2022-10-24 15:56:32 -0400

    styling project section

--- ProjectList.js
+++ ProjectList.js
@@ -3,36 +3,36 @@ const ProjectList = [
         "name": "Cat Club Bouncer",
         "description" : "The user must check each cat's ID against the criteria given by the boss of the club. Better keep up if you want to keep your job!",
         "tech": ['HTML', 'CSS', 'JavaScript'],
-        "image": "",
-        "link": ""
+        "link": "https://timrathert.github.io/Cat-Club-Bouncer/",
+        "image": "/project-images/Screenshot02.jpg"
     },
     {
         "name": "Anonimusic",
         "description" : "A place to anonymously share short audio snippets with others.",
         "tech": ['React.js','Mongo Atlas','Mongoose','Node.js','Restful Routing','Bootstrap CSS','HTML', 'CSS', 'JavaScript'],
-        "image": "",
-        "link": ""
+        "image": "/project-images/68747470733a2f2f692e696d6775722e636f6d2f7a345a4e59637a2e706e67.png",
+        "link": "https://vocal-daffodil-44e825.netlify.app/"
     },
     {    
         "name": "Knock Knock",
         "description" : "A web application that attempts to spoil your knock knock jokes and learns them if it can't.",
         "tech": ['Next.js','Python','Flask','HuggingFace APIs','','HTML', 'CSS', 'JavaScript'],
-        "image": "",
-        "link": ""
+        "link": "https://knock-knock-ten.vercel.app/auth",
+        "image": "/project-images/ss02.jpg"
     },
     {
         "name": "Dogs of Brutalism",
         "description" : "A fullstack web application dedicated to brutalist architecture and the dogs who create it.",
         "tech": ['Django','Python','PostgreSQL','Bulma','jQuery','HTML', 'CSS','JavaScript'],
-        "image": "",
-        "link": ""
+        "image": "/project-images/dogs.png",
+        "link": "https://desolate-taiga-01947.herokuapp.com/"
     },
     {
         "name": "Instant Grammification",
         "description" : "A fullstack web mock-up of Instagram's core functionality created in a collaborative sprint with 2 other developers.",
         "tech": ['MongoDB Atlas','Mongoose','Node.js','EJS','Restful Routing','Bootstrap CSS','HTML', 'CSS', 'JavaScript'],
-        "image": "",
-        "link": ""
+        "image": "/project-images/homepage2.jpg",
+        "link": "https://instant-gramifictation.herokuapp.com/home"
     },
 ]
 
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 function ProjectCard(props) {
   return (
-    <div className={`w-4/5 lg:w-2/5 min-w-[80%] md:min-w-[60%] lg:min-w-[40%]`}>
-        <h2>{props.project.name}</h2>
-        <div>{props.project.description}</div>
+    <div className={`mx-8 p-2 rounded-lg flex flex-col items-center gap-2 bg-color4 text-white w-4/5 lg:w-3/5 min-w-[80%] md:min-w-[60%] lg:min-w-[50%]`}>
+        <div className='text-xl'>{props.project.name}</div>
+        <Image layout='responsive' src={props.project.image} width='400px' height='400px' className='' />
+        <div className='bg-color5/40 p-2 rounded-lg'>{props.project.description}</div>
         <div>{props.project.tech}</div>
 
     </div>
