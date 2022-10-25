8ed77c17901441598da413f5bbc87c26000c0fe6
TimRathert <rathertt@gmail.com>
2022-10-25 10:39:33 -0400

    implementing react scroll package

--- components/Projects.js
+++ components/Projects.js
@@ -13,17 +13,12 @@ function Projects() {
         if (index === ProjectList.length-1){
             visVarRight = false
         }
-        const rightArrow = () => {
-            return (
-                <Image src = '/arrow.png' width = '400px' height = '400px' />
-            )
-        }    
         return (
             <ProjectCard project = { item } visVarLeft = {visVarLeft} visVarRight = {visVarRight} />
         )
     })
     return (
-        <div className={` justify-even h-fit w-full scroll-pl-6 overflow-x-auto lg:mx-0 p-4 mb-[200px] text-black flex flex-row rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)] transition-all`}>
+        <div className={` justify-even h-fit w-full scroll-pl-8 snap-x overflow-x-auto lg:mx-0 p-4 mb-[200px] text-black flex flex-row md:rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)] transition-all`}>
             { projCardRender }
         </div>
     )
