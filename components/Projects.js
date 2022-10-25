912c38bfff8bd7ef3c2d501828a16124e5adda23
TimRathert <rathertt@gmail.com>
2022-10-25 10:57:52 -0400

    react scroll integrated... that was easy.

--- components/Projects.js
+++ components/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
         )
     })
     return (
-        <div className={` justify-even h-fit w-full scroll-pl-8 snap-x overflow-x-auto lg:mx-0 p-4 mb-[200px] text-black flex flex-row md:rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)] transition-all`}>
+        <div id='project-container' className={` justify-even h-fit w-full scroll-pl-8 snap-x overflow-x-auto lg:mx-0 p-4 mb-[200px] text-black flex flex-row md:rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)] transition-all`}>
             { projCardRender }
         </div>
     )
