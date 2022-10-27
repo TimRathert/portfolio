d95981ce4b325d5ccccffe1861c66752b3a460db
TimRathert <rathertt@gmail.com>
2022-10-27 13:44:11 -0400

    tweak opacity of bg color for cards

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 function InfoCard(props) {
   return (
-    <div ref={props.infoDiv} id='infoCard' className={` w-full lg:mx-0 p-4 mt-32 mb-[200px] flex flex-col sm:flex-row rounded-2xl bg-color2 shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
+    <div ref={props.infoDiv} id='infoCard' className={` w-full lg:mx-0 p-4 mt-32 mb-[200px] flex flex-col sm:flex-row rounded-2xl bg-color2/80 shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
       <Image objectFit='cover' src='/me.jpg'height={`400px`} width={`400px`} className={`aspect-square rounded-full shadow-[2px_2px_4px_rgba(0,0,0,0.3)]`} alt="Tim Rathert"/>
 
       <div className='text-color6 bg-fixed bg-containerbg w-full rounded-xl p-2 m-1 flex-col place-content-center sepia-[50%]'>
