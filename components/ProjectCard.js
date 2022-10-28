81cc0046210d4eb4aa3873c38ad60becabe49bbd
TimRathert <rathertt@gmail.com>
2022-10-28 12:48:34 -0400

    phrasing, lana

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -14,7 +14,7 @@ function ProjectCard(props) {
             <a href={props.project.link} target="_blank" rel='noreferrer' ><Image objectFit='cover' src={props.project.image} width='400px' height='400px' className='shadow-[2px_2px_5px_rgba(0,0,0,0.2)]' alt="Project Screenshot" /></a>
             <div className=' md:w-5/6 bg-color6/80 p-2 rounded-lg shadow-[2px_2px_5px_rgba(0,0,0,0.2)]'>{props.project.description}</div>
             <div className={` md:w-5/6 flex flex-row flex-wrap`}>{techList}</div>
-            <div className={`m-1 p-1 bg-color1/50 p-1 rounded-md hover:bg-color5/80 transition-colors`}><a href={props.project.github} target="_blank" rel='noreferrer' >Repo Link</a></div>
+            <div className={`m-1 p-1 bg-color1/50 p-1 rounded-md hover:bg-color5/80 transition-colors`}><a href={props.project.github} target="_blank" rel='noreferrer' >Github Repo</a></div>
 
         </div>
   )
