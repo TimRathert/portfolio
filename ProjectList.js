fa2c2aa22c74c08e6f5134db863b0c85cdbd1ef7
TimRathert <rathertt@gmail.com>
2022-10-28 12:25:43 -0400

    added github links to projects

--- ProjectList.js
+++ ProjectList.js
@@ -3,16 +3,18 @@ const ProjectList = [
     {    
         "name": "Knock Knock",
         "description" : "A web application that attempts to spoil your knock knock jokes and learns them if it can't.",
-        "tech": ['Next.js','Python','Flask','HuggingFace APIs','HTML', 'CSS', 'JavaScript'],
+        "tech": ['Next.js','Python','Flask','Machine Learning','Sentence Transformers','HTML', 'CSS', 'JavaScript'],
         "link": "https://knock-knock-ten.vercel.app/auth",
-        "image": "/project-images/ss02.jpg"
+        "image": "/project-images/ss02.jpg",
+        "github": "https://github.com/TimRathert/knock-knock"
     },
     {
         "name": "Cat Club Bouncer",
         "description" : "The user must check each cat's ID against the criteria given by the boss of the club. Better keep up if you want to keep your job!",
         "tech": ['HTML', 'CSS', 'JavaScript'],
         "link": "https://timrathert.github.io/Cat-Club-Bouncer/",
-        "image": "/project-images/Screenshot02.jpg"
+        "image": "/project-images/Screenshot02.jpg",
+        "github": "https://github.com/TimRathert/Cat-Club-Bouncer"
     },
 
     {
@@ -20,21 +22,24 @@ const ProjectList = [
         "description" : "A fullstack web application dedicated to brutalist architecture and the dogs who create it.",
         "tech": ['Django','Python','PostgreSQL','Bulma','jQuery','HTML', 'CSS','JavaScript'],
         "image": "/project-images/dogs.png",
-        "link": "https://desolate-taiga-01947.herokuapp.com/"
+        "link": "https://desolate-taiga-01947.herokuapp.com/",
+        "github": "https://github.com/TimRathert/django-dogs"
     },
     {
         "name": "Anonimusic",
         "description" : "A place to anonymously share short audio clips with others.",
         "tech": ['React.js','Mongo Atlas','Mongoose','Node.js','Restful Routing','Bootstrap CSS','HTML', 'CSS', 'JavaScript'],
         "image": "/project-images/68747470733a2f2f692e696d6775722e636f6d2f7a345a4e59637a2e706e67.png",
-        "link": "https://vocal-daffodil-44e825.netlify.app/"
+        "link": "https://vocal-daffodil-44e825.netlify.app/",
+        "github": "https://github.com/TimRathert/anonimusic_frontend_deployment"
     },
     {
         "name": "Instant Grammification",
         "description" : "A fullstack web mock-up of Instagram's core functionality created in a collaborative sprint with 2 other developers.",
         "tech": ['MongoDB Atlas','Mongoose','Node.js','EJS','Restful Routing','Bootstrap CSS','HTML', 'CSS', 'JavaScript'],
         "image": "/project-images/homepage2.jpg",
-        "link": "https://instant-gramifictation.herokuapp.com/home"
+        "link": "https://instant-gramifictation.herokuapp.com/home",
+        "github": "https://github.com/TimRathert/Project-2"
     },
 ]
 
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
+
 function ProjectCard(props) {
     const techList = props.project.tech.map((item, index)=>{
         return (
@@ -8,13 +9,14 @@ function ProjectCard(props) {
         )
     })
   return (
-        <a key ={ props.index } href={props.project.link} target="_blank" rel='noreferrer ' className={`select-none snap-start mx-8 p-2 rounded-lg flex flex-col items-center gap-2 bg-color4 text-white w-4/5 lg:w-3/5 min-w-[80%] md:min-w-[60%] lg:min-w-[50%] shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
+        <div key ={ props.index } className={`select-none snap-start mx-8 p-2 rounded-lg flex flex-col items-center gap-2 bg-color4 text-white w-4/5 lg:w-3/5 min-w-[80%] md:min-w-[60%] lg:min-w-[50%] shadow-[2px_2px_5px_rgba(0,0,0,0.2)]`}>
             <div className='text-xl'>{props.project.name}</div>
-            <Image objectFit='cover' src={props.project.image} width='400px' height='400px' className='shadow-[2px_2px_5px_rgba(0,0,0,0.2)]' alt="Project Screenshot" />
+            <a href={props.project.link} target="_blank" rel='noreferrer' ><Image objectFit='cover' src={props.project.image} width='400px' height='400px' className='shadow-[2px_2px_5px_rgba(0,0,0,0.2)]' alt="Project Screenshot" /></a>
             <div className=' md:w-5/6 bg-color6/80 p-2 rounded-lg shadow-[2px_2px_5px_rgba(0,0,0,0.2)]'>{props.project.description}</div>
             <div className={` md:w-5/6 flex flex-row flex-wrap`}>{techList}</div>
+            <div className={`m-1 p-1 bg-color1/50 p-1 rounded-md hover:bg-color5/80 transition-colors`}><a href={props.project.github} target="_blank" rel='noreferrer' >Repo Link</a></div>
 
-        </a>
+        </div>
   )
 }
 
